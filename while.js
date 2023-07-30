f9f400f4afa767447c924d9e4c42d3a0584c0253
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-30 23:28:18 +0600

    Loop Example numbers, odd numbers, even numbers

--- while.js
+++ while.js
@@ -4,10 +4,16 @@
 // roastGiven = roastGiven +1;
 // roastGiven += 1;
 
+/**
+ * 1. Loop variable
+ * 2. Condition inside while
+ * 3. Loop body
+ * 4. Change the loop variable
+ */
 var roastGiven = 0;
 while (roastGiven < 7) {
     console.log('Roast Den, Please!!')
     console.log(roastGiven);
-    roastGiven++;
+    // roastGiven++;
    
 }
