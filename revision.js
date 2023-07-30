f3aea6181e678827a69dde146f87028f128050ef
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-30 21:51:43 +0600

    revisonsVariable array and conditionals revision

---
+++ revision.js
@@ -0,0 +1,25 @@
+// string type variable
+var bottle = 'water';
+var bottleColor = 'yellow';
+// nuber type variable
+var sunglassPrice = 120;
+
+var panColor = 'blue';
+// boolean type variable
+var isExpensivfe = true;
+
+var tableItems = ['bottle', 'pen', 'mouse', 'sunglass', 'paper'];
+
+var item3 = tableItems[3];
+tableItems[4] = 'notebook';
+var penIndex = tableItems.indexOf('pen')
+// push pop
+
+// conditionslas
+
+if(tableItems.length > 4) {
+
+}
+else{
+    
+}
