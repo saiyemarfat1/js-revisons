782135e43f34316c21b79023abd35273b8832d6c
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-30 22:15:24 +0600

    While loop, debug and understand while loop

---
+++ while.js
@@ -0,0 +1,13 @@
+// if(condition){
+
+// }
+// roastGiven = roastGiven +1;
+// roastGiven += 1;
+
+var roastGiven = 0;
+while (roastGiven < 7) {
+    console.log('Roast Den, Please!!')
+    console.log(roastGiven);
+    roastGiven++;
+   
+}
