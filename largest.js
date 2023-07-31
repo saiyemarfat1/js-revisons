d16e8aa7d2fb3e07c02183541c4992fc51bd7ecd
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-31 13:34:15 +0600

    Practice Loop and assess your current JS Level

---
+++ largest.js
@@ -0,0 +1,20 @@
+var num1 = 3;
+var num2 = 9;
+var num3 = 45;
+
+if (num1 > num2){
+    if (num1 > num3){
+        console.log(num1);
+    }
+    else{
+        console.log(num3);
+    }
+}
+else{
+    if(num2 > num3){
+        console.log(num3);
+    }
+    else{
+      console.log(num3);  
+    }
+}
---
+++ loop.js
@@ -0,0 +1,16 @@
+/* 
+-------------------
+LOOP
+-------------------
+1. Display  "Ajke amar mon valo nei" for 39 times 
+2. Display numbers between 58 to 97
+3. Show all even numbers from 412 to 456
+4. Show all odd numbers 581 to 623
+5. Difference between while loop and for loop
+6. Declare an array for all the topics that you hav learned last few days display than as output
+7. Create an array for all the mobile phones. Display all the elements of  odd the array by using a while loop
+8. Run a loop from 30 to 86. this loop will stop if the values get heigher than 44.
+9. Write the price of the books that you have. Display the prices if the prices is lower than 200.
+
+
+*/
