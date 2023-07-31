4cc8ba791dce8f2dc90fa65f5e49f3e5d6be5ae6
Saiyem Arfat <saiyemarfat775@gmail.com>
2023-07-31 13:05:10 +0600

    Subtraction, average and remainder practice Problem

---
+++ apple-orange.js
@@ -0,0 +1,8 @@
+var moneyGiven = 1000;
+var applePrice = 400;
+var orangePrice = 300;
+
+var totalPrice = applePrice + orangePrice;
+console.log(totalPrice);
+var giveBack = moneyGiven - totalPrice;
+console.log(giveBack);
